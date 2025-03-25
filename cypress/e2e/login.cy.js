33eebf25b0e2c0a962c5503957db953ea92e650a
andrew-moscheni <agmoscheni@gmail.com>
2025-03-25 14:21:21 -0400

    added wait commands to delay

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,12 +6,14 @@ describe('User Login', () => {
     it('should login with valid credentials', function() {
         const currentUser = this.users.validUser;
         cy.login(currentUser.email, currentUser.password);
-        cy.url({timeout: 10000}).should("eq",'http://localhost:5173/dashboard');
+        cy.wait(5000);
+        cy.url().should("eq",'http://localhost:5173/dashboard');
     });
   
     it('should show error for invalid credentials', function() {
         const invalid = this.users.invalidUser;
         cy.login(invalid.email, invalid.password);
-        cy.url({timeout:10000}).get('#error-msg', {timeout:2000}).should('not.include', 'Error:');
+        cy.wait(5000);
+        cy.url().get('#error-msg').should('not.include', 'Error:');
     });
   });
