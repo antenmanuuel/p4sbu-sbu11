2a1e397b4e23b4e3c843f11c498164ff185cbc12
andrew-moscheni <agmoscheni@gmail.com>
2025-03-25 12:48:32 -0400

    added even more delays to the cypress tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,12 +6,12 @@ describe('User Login', () => {
     it('should login with valid credentials', function() {
         const currentUser = this.users.validUser;
         cy.login(currentUser.email, currentUser.password);
-        cy.url({timeout: 2000}).should("eq",'http://localhost:5173/dashboard');
+        cy.url({timeout: 10000}).should("eq",'http://localhost:5173/dashboard');
     });
   
     it('should show error for invalid credentials', function() {
         const invalid = this.users.invalidUser;
         cy.login(invalid.email, invalid.password);
-        cy.get('#error-msg', {timeout:2000}).should('not.include', 'Error:');
+        cy.url({timeout:10000}).get('#error-msg', {timeout:2000}).should('not.include', 'Error:');
     });
   });
