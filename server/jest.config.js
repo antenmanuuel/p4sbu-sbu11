1f6891f3e365304d1f328d82dd1aab433c85e9c7
Anten Manuuel <anten.manuuel@stonybrook.edu>
2025-03-25 23:38:40 -0400

    add tests

---
+++ server/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.js'],
+    testTimeout: 10000,
+    bail: false,
+    verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'routes/**/*.js',
+        'controllers/**/*.js',
+        'models/**/*.js',
+        '!**/node_modules/**',
+        '!**/tests/**',
+        '!routes/lot.js',
+        '!routes/statistics.js'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'clover'],
+    clearMocks: true,
+    restoreMocks: true,
+    resetModules: true,
+}; 
